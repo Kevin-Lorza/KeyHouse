447ba6431b5b7398de44a9937f5f10dff5829b76
Samuel <escobar.samuel@correounivalle.edu.co>
2025-04-12 14:25:45 -0500

    Acutalizo el index html y el Appjs

--- KeyHouse-FRONT/src/App.js
+++ KeyHouse-FRONT/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CreateAccount from './components/CreateAccount';
-import Login from './components/Login';
-import ForgotPassword from './components/ForgotPassword';
-import Home from './components/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateAccount from "./components/CreateAccount";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
+import Home from "./components/Home";
 import RegistrarCasa from "./components/RegistrarCasa"; // Importar la nueva página
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/RegistrarCasa" element={<RegistrarCasa />} />
+        <Route path="/perfil" element={<Profile />} />
         <Route path="/" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
